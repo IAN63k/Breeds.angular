4f4091b2b25b536863d5e1ac426f2affbe3c1c3c
julian <julian63k@gmail.com>
2025-05-12 00:20:19 -0500

    solve problem with the sidebar.  state: sidebar-incomplete 🥲

--- src/app/features/breeds/breeds.module.ts
+++ src/app/features/breeds/breeds.module.ts
@@ -5,28 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { BreedListComponent } from './breed-list/breed-list.component';
 import { FavoritesComponent } from '../favorites/favorites.component';
-import { ShellComponent } from './components/shell/shell.component';
-  
+
 
 const routes: Routes = [
   { path: '', component: BreedListComponent },
   { path: 'favorites', component: FavoritesComponent },
-  { path: 'shell', component: ShellComponent }
   { path: 'breeds', component: BreedListComponent },
 ];
-
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(routes),
-
-    BreedListComponent,
-    FavoritesComponent,
-    ShellComponent
-  ]
-})
 
 export class BreedsModule { }
 
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'; // o provideNoopAnimations
-// import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { ShellComponent } from './app/features/breeds/components/shell/shell.component';
+import { AppComponent } from './app/app.component';
 
 /*  interceptores propios, si los tienes  */
 // import { apiKeyInterceptor } from './app/core/interceptors/api-key.interceptor';
 
-bootstrapApplication(ShellComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),   // 👈  OBLIGATORIO
     // provideInterceptor(apiKeyInterceptor),       // (ejemplo)
